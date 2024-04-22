5002d856542028cf846b48320a37bd56890f79fb
nickroh <roh517@naver.com>
2024-04-22 20:17:22 +0900

    Initial Commit

---
+++ musicSelector/musicSelectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  musicSelectorApp.swift
+//  musicSelector
+//
+//  Created by Geonho Roh on 4/22/24.
+//
+
+import SwiftUI
+
+@main
+struct musicSelectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
